da4f01ad0742ab55bd7dd7a745c03d103d8bc38b
Hugo Zanini <hugozaninigomes@gmail.com>
2022-03-12 20:24:20 +0000

    Creating js files

---
+++ src/index.js
@@ -0,0 +1,172 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as tf from "@tensorflow/tfjs";
+import { loadGraphModel } from "@tensorflow/tfjs-converter";
+import "./styles.css";
+tf.setBackend("webgl");
+
+const threshold = 0.6;
+
+async function load_model() {
+  // It's possible to load the model locally or from a repo
+  // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
+  //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
+
+  const model = await loadGraphModel(
+    "https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/sku/models/sku80/model.json"
+  );
+  return model;
+}
+
+let classesDir = {
+  1: {
+    name: "SKU",
+    id: 1
+  },
+  2: {
+    name: "Other",
+    id: 2
+  }
+};
+
+class App extends React.Component {
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "environment"
+          }
+        })
+        .then((stream) => {
+          window.stream = stream;
+          this.videoRef.current.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+
+      const modelPromise = load_model();
+
+      Promise.all([modelPromise, webCamPromise])
+        .then((values) => {
+          this.detectFrame(this.videoRef.current, values[0]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }
+
+  detectFrame = (video, model) => {
+    tf.engine().startScope();
+    model.executeAsync(this.process_input(video)).then((predictions) => {
+      this.renderPredictions(predictions, video);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
+      tf.engine().endScope();
+    });
+  };
+
+  process_input(video_frame) {
+    const tfimg = tf.browser.fromPixels(video_frame).toInt();
+    const expandedimg = tfimg.transpose([0, 1, 2]).expandDims();
+    return expandedimg;
+  }
+
+  buildDetectedObjects(scores, threshold, boxes, classes, classesDir) {
+    const detectionObjects = [];
+    var video_frame = document.getElementById("frame");
+
+    scores[0].forEach((score, i) => {
+      if (score > threshold) {
+        const bbox = [];
+        const minY = boxes[0][i][0] * video_frame.offsetHeight;
+        const minX = boxes[0][i][1] * video_frame.offsetWidth;
+        const maxY = boxes[0][i][2] * video_frame.offsetHeight;
+        const maxX = boxes[0][i][3] * video_frame.offsetWidth;
+        bbox[0] = minX;
+        bbox[1] = minY;
+        bbox[2] = maxX - minX;
+        bbox[3] = maxY - minY;
+        detectionObjects.push({
+          class: classes[i],
+          label: "SKU", //classesDir[classes[i]].name,
+          score: score.toFixed(4),
+          bbox: bbox
+        });
+      }
+    });
+    return detectionObjects;
+  }
+
+  renderPredictions = (predictions) => {
+    const ctx = this.canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    //Getting predictions
+    const boxes = predictions[4].arraySync();
+    const scores = predictions[7].arraySync();
+    const classes = predictions[2].dataSync();
+    const detections = this.buildDetectedObjects(
+      scores,
+      threshold,
+      boxes,
+      classes,
+      classesDir
+    );
+
+    detections.forEach((item) => {
+      const x = item["bbox"][0];
+      const y = item["bbox"][1];
+      const width = item["bbox"][2];
+      const height = item["bbox"][3];
+
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FF00";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(x, y, width, height);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Real-Time Object Detection: SKUS</h1>
+        <h3>MobileNetV2</h3>
+        <video
+          style={{ height: "600px", width: "500px" }}
+          className="size"
+          autoPlay
+          playsInline
+          muted
+          ref={this.videoRef}
+          width="600"
+          height="500"
+          id="frame"
+        />
+        <canvas
+          className="size"
+          ref={this.canvasRef}
+          width="600"
+          height="500"
+        />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
